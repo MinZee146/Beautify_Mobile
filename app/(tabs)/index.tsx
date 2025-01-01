8ed2df6c854822bcd09b31933ae635da616a7c67
MinZee146 <minhduy146204@gmail.com>
2025-01-01 23:48:46 +0700

    Add fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,7 +4,16 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View>
-      <Text>About screen</Text>
+      <Text
+        style={{
+          fontSize: 16,
+          color: "#000",
+          fontWeight: "bold",
+          fontFamily: "Poppins",
+        }}
+      >
+        About screen
+      </Text>
       <Link href="/promo">Go to About screen</Link>
     </View>
   );
