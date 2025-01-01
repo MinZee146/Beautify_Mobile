f52e540d02f17a8b4d33c955811098640b48b61e
MinZee146 <minhduy146204@gmail.com>
2025-01-01 17:51:51 +0700

    Import Tamgui

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,26 @@
 import { Text, View } from "react-native";
+import { TamaguiProvider, createTamagui } from "@tamagui/core";
+import { config } from "@tamagui/config/v3";
+
+const tamaguiConfig = createTamagui(config);
+
+type Conf = typeof tamaguiConfig;
+declare module "@tamagui/core" {
+  interface TamaguiCustomConfig extends Conf {}
+}
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <TamaguiProvider config={tamaguiConfig}>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+      </View>
+    </TamaguiProvider>
   );
 }
