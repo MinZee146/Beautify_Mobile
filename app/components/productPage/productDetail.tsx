258e1f496a94e4bc2a46d46a9c4c55193df658b6
Nguyen Hoang Hien <hien20040908@gmail.com>
2025-01-04 02:58:33 +0700

    update product detail

--- app/components/productPage/productDetail.tsx
+++ app/components/productPage/productDetail.tsx
@@ -84,152 +84,151 @@ const ProductDetailScreen = () => {
           <Ionicons name="bag-handle-outline" size={24} color="#FBFCD4" />
         </TouchableOpacity>
       </XStack>
-      <View style={styles.imageContainer}>
-        <Swiper
-          style={styles.imageSwiper}
-          showsButtons={false}
-          autoplay={true}
-          autoplayTimeout={3}
-        >
-          {product.images.map((image, index) => (
-            <Image
-              key={index}
-              source={{ uri: image }}
-              style={styles.productImage}
-            />
-          ))}
-        </Swiper>
-      </View>
 
       {/* Phần dưới là chi tiết sản phẩm */}
-      <ScrollView contentContainerStyle={styles.content}>
-        <View style={styles.headerRow}>
+      <ScrollView>
+        <View style={styles.imageContainer}>
+          <Swiper
+            style={styles.imageSwiper}
+            showsButtons={false}
+            autoplay={true}
+            autoplayTimeout={5}
+          >
+            {product.images.map((image, index) => (
+              <Image
+                key={index}
+                source={{ uri: image }}
+                style={styles.productImage}
+              />
+            ))}
+          </Swiper>
+        </View>
+        <View style={styles.scrollContent}>
+          <View style={styles.tagContainer}>
+            <View style={styles.brandItem}>
+              <Text style={styles.tagText}>{product.brand}</Text>
+            </View>
+            <View style={styles.categoryItem}>
+              <Text style={styles.tagText}>{product.category}</Text>
+            </View>
+          </View>
           <Text style={styles.productName}>{product.name}</Text>
           <Text style={styles.productPrice}>{product.price}</Text>
-        </View>
 
-        <View style={styles.tagContainer}>
-          <View style={styles.brandItem}>
-            <Text style={styles.tagText}>{product.brand}</Text>
-          </View>
-          <View style={styles.categoryItem}>
-            <Text style={styles.tagText}>{product.category}</Text>
-          </View>
-        </View>
-
-        <Text style={styles.productRating}>
-          Rating: {product.rating} ⭐ ({product.reviews} reviews)
-        </Text>
-        <Text style={styles.productDescription}>
-          {showMore
-            ? product.description
-            : `${product.description.substring(0, 100)}...`}
-        </Text>
-        {product.description.length > 100 && (
-          <TouchableOpacity onPress={() => setShowMore(!showMore)}>
-            <Text style={styles.moreButton}>
-              {showMore ? "Show Less" : "Show More"}
-            </Text>
-          </TouchableOpacity>
-        )}
-
-        {/* Khối nút Add to Cart và điều chỉnh số lượng */}
-        <View style={styles.cartSection}>
-          <TouchableOpacity style={styles.addToCartButton}>
-            <Text style={styles.addToCartText}>Add to Cart</Text>
-          </TouchableOpacity>
-
-          {/* Phần tăng giảm số lượng */}
-          <View style={styles.quantityContainer}>
-            <TouchableOpacity
-              onPress={decreaseQuantity}
-              style={styles.quantityButton}
-            >
-              <Text style={styles.quantityText}>-</Text>
+          <Text style={styles.productRating}>
+            Rating: {product.rating} ⭐ ({product.reviews} reviews)
+          </Text>
+          <Text style={styles.productDescription}>
+            {showMore
+              ? product.description
+              : `${product.description.substring(0, 100)}...`}
+          </Text>
+          {product.description.length > 100 && (
+            <TouchableOpacity onPress={() => setShowMore(!showMore)}>
+              <Text style={styles.moreButton}>
+                {showMore ? "Show Less" : "Show More"}
+              </Text>
             </TouchableOpacity>
-            <TextInput
-              style={styles.quantityInput}
-              value={String(quantity)}
-              keyboardType="numeric"
-              editable={false}
-            />
-            <TouchableOpacity
-              onPress={increaseQuantity}
-              style={styles.quantityButton}
-            >
-              <Text style={styles.quantityText}>+</Text>
+          )}
+
+          {/* Khối nút Add to Cart và điều chỉnh số lượng */}
+          <View style={styles.cartSection}>
+            <TouchableOpacity style={styles.addToCartButton}>
+              <Text style={styles.addToCartText}>Add to Cart</Text>
             </TouchableOpacity>
+
+            {/* Phần tăng giảm số lượng */}
+            <View style={styles.quantityContainer}>
+              <TouchableOpacity
+                onPress={decreaseQuantity}
+                style={styles.quantityButton}
+              >
+                <Text style={styles.quantityText}>-</Text>
+              </TouchableOpacity>
+              <TextInput
+                style={styles.quantityInput}
+                value={String(quantity)}
+                keyboardType="numeric"
+                editable={false}
+              />
+              <TouchableOpacity
+                onPress={increaseQuantity}
+                style={styles.quantityButton}
+              >
+                <Text style={styles.quantityText}>+</Text>
+              </TouchableOpacity>
+            </View>
           </View>
-        </View>
 
-        {/* Phần Tổng Review */}
-        <View style={styles.commentSection}>
-          {/* Số lượng review và Write Review button */}
-          <View style={styles.reviewHeader}>
-            <Text style={styles.reviewCount}>{product.reviews} Reviews</Text>
-            <TouchableOpacity style={styles.writeReviewButton}>
-              <Text style={styles.writeReviewText}>Write Review</Text>
-            </TouchableOpacity>
-          </View>
+          {/* Phần Tổng Review */}
+          <View style={styles.commentSection}>
+            {/* Số lượng review và Write Review button */}
+            <View style={styles.reviewHeader}>
+              <Text style={styles.reviewCount}>{product.reviews} Reviews</Text>
+              <TouchableOpacity style={styles.writeReviewButton}>
+                <Text style={styles.writeReviewText}>Write Review</Text>
+              </TouchableOpacity>
+            </View>
 
-          {/* Hiển thị tổng số rating lớn bên trái */}
-          <View style={styles.ratingStats}>
-            <Text style={styles.totalRatingText}>{product.rating} ⭐</Text>
+            {/* Hiển thị tổng số rating lớn bên trái */}
+            <View style={styles.ratingStats}>
+              <Text style={styles.totalRatingText}>{product.rating} ⭐</Text>
 
-            {/* Thống kê rating (1, 2, 3, 4, 5 sao) nằm bên phải */}
-            <View style={styles.ratingDistribution}>
-              {Object.entries(product.ratingsCount).map(([rating, count]) => (
-                <View key={rating} style={styles.ratingItem}>
-                  <Text style={styles.ratingText}>
-                    {rating} ⭐
-                    <Text style={styles.ratingCount}> ({count})</Text>
-                  </Text>
+              {/* Thống kê rating (1, 2, 3, 4, 5 sao) nằm bên phải */}
+              <View style={styles.ratingDistribution}>
+                {Object.entries(product.ratingsCount).map(([rating, count]) => (
+                  <View key={rating} style={styles.ratingItem}>
+                    <Text style={styles.ratingText}>
+                      {rating} ⭐
+                      <Text style={styles.ratingCount}> ({count})</Text>
+                    </Text>
 
-                  {/* Thanh tiến trình cho mức độ đánh giá */}
-                  <View style={styles.progressBarContainer}>
-                    <View
-                      style={[
-                        styles.progressBar,
-                        {
-                          width: `${getRatingPercentage(
-                            rating as unknown as 1 | 2 | 3 | 4 | 5
-                          )}%`,
-                        },
-                      ]}
-                    />
+                    {/* Thanh tiến trình cho mức độ đánh giá */}
+                    <View style={styles.progressBarContainer}>
+                      <View
+                        style={[
+                          styles.progressBar,
+                          {
+                            width: `${getRatingPercentage(
+                              rating as unknown as 1 | 2 | 3 | 4 | 5
+                            )}%`,
+                          },
+                        ]}
+                      />
+                    </View>
                   </View>
-                </View>
-              ))}
+                ))}
+              </View>
             </View>
           </View>
-        </View>
 
-        {/* Phần thông tin Review của User (mới thêm) */}
-        <View style={styles.userReviews}>
-          {/* 1. Thông tin user và nút chỉnh sửa */}
-          <View style={styles.userHeader}>
-            <View style={styles.userInfo}>
-              <Text style={styles.userName}>User Name</Text>
-              <Text style={styles.userEmail}>user@example.com</Text>
+          {/* Phần thông tin Review của User (mới thêm) */}
+          <View style={styles.userReviews}>
+            {/* 1. Thông tin user và nút chỉnh sửa */}
+            <View style={styles.userHeader}>
+              <View style={styles.userInfo}>
+                <Text style={styles.userName}>User Name</Text>
+                <Text style={styles.userEmail}>user@example.com</Text>
+              </View>
+              <TouchableOpacity style={styles.optionsButton}>
+                <Text style={styles.optionsText}>...</Text>
+              </TouchableOpacity>
             </View>
-            <TouchableOpacity style={styles.optionsButton}>
-              <Text style={styles.optionsText}>...</Text>
-            </TouchableOpacity>
-          </View>
 
-          {/* 2. Ngôi sao và tiêu đề đánh giá */}
-          <View style={styles.ratingWithTitle}>
-            <Text style={styles.starRating}>⭐⭐⭐⭐⭐</Text>
-            <Text style={styles.reviewTitle}>Great Product!</Text>
-          </View>
+            {/* 2. Ngôi sao và tiêu đề đánh giá */}
+            <View style={styles.ratingWithTitle}>
+              <Text style={styles.starRating}>⭐⭐⭐⭐⭐</Text>
+              <Text style={styles.reviewTitle}>Great Product!</Text>
+            </View>
 
-          {/* 3. Comment và thời gian */}
-          <View style={styles.commentWithTime}>
-            <Text style={styles.userComment}>
-              This is a great product, totally worth the price. Highly
-              recommended!
-            </Text>
-            <Text style={styles.reviewTime}>2 hours ago</Text>
+            {/* 3. Comment và thời gian */}
+            <View style={styles.commentWithTime}>
+              <Text style={styles.userComment}>
+                This is a great product, totally worth the price. Highly
+                recommended!
+              </Text>
+              <Text style={styles.reviewTime}>2 hours ago</Text>
+            </View>
           </View>
         </View>
       </ScrollView>
@@ -242,36 +241,23 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
   },
-  imageContainer: {
-    height: 300,
-  },
-  imageSwiper: {
-    height: "100%",
-  },
-  productImage: {
-    width: "100%",
-    height: "100%",
-    resizeMode: "cover",
-  },
-  content: {
+  imageContainer: { width: "100%", aspectRatio: 1 },
+  imageSwiper: { height: "100%" },
+  productImage: { width: "100%", aspectRatio: 1, resizeMode: "cover" },
+  scrollContent: {
     padding: 16,
-    paddingBottom: 100,
-  },
-  headerRow: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginBottom: 8,
   },
   productName: {
     fontSize: 24,
     fontWeight: "bold",
     flex: 1,
-    marginRight: 8,
+    marginBottom: 4,
   },
   productPrice: {
     fontSize: 20,
     color: "#e91e63",
-    marginBottom: 8,
+    fontWeight: "bold",
+    marginBottom: 4,
   },
   tagContainer: {
     flexDirection: "row",
@@ -280,16 +266,16 @@ const styles = StyleSheet.create({
   },
   brandItem: {
     backgroundColor: "#e3f2fd",
-    borderRadius: 12,
-    paddingVertical: 4,
-    paddingHorizontal: 8,
+    borderRadius: 24,
+    paddingVertical: 10,
+    paddingHorizontal: 16,
     marginRight: 8,
   },
   categoryItem: {
     backgroundColor: "#f1f1f1",
-    borderRadius: 12,
-    paddingVertical: 4,
-    paddingHorizontal: 8,
+    borderRadius: 24,
+    paddingVertical: 10,
+    paddingHorizontal: 16,
   },
   tagText: {
     fontSize: 14,
