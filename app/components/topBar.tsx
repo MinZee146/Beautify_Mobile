410180501bd69cf1d041233c2360fee4148d65b4
Nguyen Hoang Hien <hien20040908@gmail.com>
2025-01-03 19:03:30 +0700

    add logo

---
+++ app/components/topBar.tsx
@@ -0,0 +1,10 @@
+      <XStack gap={8} alignItems="center">
+        <Image
+          source={require("../../assets/images/AppIcon.jpg")}
+          width={36}
+          height={36}
+          borderRadius={12}
+        />
+        <Text fontSize={20} fontWeight="bold" color="#FF748B">
+          BEAUTIFY
+        </Text>
